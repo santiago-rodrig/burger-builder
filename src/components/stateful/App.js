c6007329152b37d41cb618d4435a2013b2221396
santiago-rodrig <santo1996.29@gmail.com>
2020-12-22 14:58:00 -0400

    define layout component

--- src/components/stateful/App.js
+++ src/components/stateful/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import Layout from "../stateless/Layout";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>App Component</h1>
-      </div>
+      <Layout>
+        <p>Does this work?</p>
+      </Layout>
     );
   }
 }
