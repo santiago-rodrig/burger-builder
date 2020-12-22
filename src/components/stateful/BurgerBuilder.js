9f07deb257c5fec101d20ee6c7339b33844b1e84
santiago-rodrig <santo1996.29@gmail.com>
2020-12-22 16:52:35 -0400

    dynamically render burger ingredients

--- src/components/stateful/BurgerBuilder.js
+++ src/components/stateful/BurgerBuilder.js
@@ -3,10 +3,23 @@ import Aux from "../higuerOrder/Aux";
 import Burger from "../stateless/Burguer";
 
 class BurgerBuilder extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ingredients: [
+        { type: "salad", quantity: 1 },
+        { type: "bacon", quantity: 1 },
+        { type: "cheese", quantity: 2 },
+        { type: "meat", quantity: 2 },
+      ],
+    };
+  }
+
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Burger controls</div>
       </Aux>
     );
--- src/components/stateless/Burguer.js
+++ src/components/stateless/Burguer.js
@@ -1,16 +1,38 @@
 import React from "react";
 import BurgerIngredient from "./BurgerIngredient";
 import classes from "../../styles/Burger.css";
+import PropTypes from "prop-types";
 
 const Burger = (props) => {
+  const ingredientsJsx = props.ingredients.reduce((accumulator, ingredient) => {
+    for (let i = 0; i < ingredient.quantity; i += 1) {
+      accumulator.push(
+        <BurgerIngredient
+          key={`${ingredient.type}${i}`}
+          type={ingredient.type}
+        />
+      );
+    }
+
+    return accumulator;
+  }, []);
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {ingredientsJsx}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
+};
+
+Burger.propTypes = {
+  ingredients: PropTypes.arrayOf(
+    PropTypes.exact({
+      type: PropTypes.string,
+      quantity: PropTypes.number,
+    })
+  ).isRequired,
 };
 
 export default Burger;
